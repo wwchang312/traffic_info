94daa2b598f5a6228ded5887810a1b2268d97c62
wwchang <wanw95@gmail.com>
2026-07-12 16:01:02 +0900

    서울시 교통량정보 호출 API 최초 등록

---
+++ airflow/dag/DAG_Seoul_traffic_info.py
@@ -0,0 +1,29 @@
+from airflow.sdk import DAG,Variable
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+import pendulum
+from airflow.decorators import task
+import requests
+import xmltodict
+import boto3
+import json
+
+with DAG(
+    dag_id='DAG_Seoul_traffic_info',
+    schedule=None,
+    start_date=pendulum.datetime(2026, 6, 1,tz='Asia/Seoul'),
+    tags=['서울시','교통량'],
+    description='서울시 교통량 정보 호출 API',
+    catchup= False
+) as dag:
+
+    hook = S3Hook(aws_conn_id='minio_connection')
+
+    content = hook.read_key(
+        bucket_name="spot-info",
+        key="spot_name/20260710_data.json",
+    )
+
+    spot_list = json.loads(content)
+
+
+    print(spot_list)
